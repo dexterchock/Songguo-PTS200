b943350ee703ff0088ef3636fbbdc8c0681fa7cb
dexterchock <122592855+dexterchock@users.noreply.github.com>
2026-09-19 20:51:58 +0800

    Refactor EEPROM address definitions and functions

--- SolderingPen_ESP32S2/UtilsEEPROM.h
+++ SolderingPen_ESP32S2/UtilsEEPROM.h
@@ -5,28 +5,28 @@
 #include <string.h>
 
 #define ADDR_SYSTEM_INIT_FLAG (0)
-#define ADDR_DEFAULT_TEMP (ADDR_SYSTEM_INIT_FLAG + 4)
-#define ADDR_SLEEP_TEMP (ADDR_DEFAULT_TEMP + 2)
-#define ADDR_BOOST_TEMP (ADDR_SLEEP_TEMP + 2)
-#define ADDR_TIME_2_SLEEP (ADDR_BOOST_TEMP + 1)
-#define ADDR_TIME_2_OFF (ADDR_TIME_2_SLEEP + 2)
-#define ADDR_TIME_OF_BOOST (ADDR_TIME_2_OFF + 1)
-#define ADDR_MAIN_SCREEN (ADDR_TIME_OF_BOOST + 1)
-#define ADDR_PID_ENABLE (ADDR_MAIN_SCREEN + 1)
-#define ADDR_BEEP_ENABLE (ADDR_PID_ENABLE + 1)
-#define ADDR_VOLTAGE_VALUE (ADDR_BEEP_ENABLE + 1)
-#define ADDR_QC_ENABLE (ADDR_VOLTAGE_VALUE + 1)
+#define ADDR_DEFAULT_TEMP     (ADDR_SYSTEM_INIT_FLAG + 4)
+#define ADDR_SLEEP_TEMP       (ADDR_DEFAULT_TEMP + 2)
+#define ADDR_BOOST_TEMP       (ADDR_SLEEP_TEMP + 2)
+#define ADDR_TIME_2_SLEEP     (ADDR_BOOST_TEMP + 1)
+#define ADDR_TIME_2_OFF       (ADDR_TIME_2_SLEEP + 2)
+#define ADDR_TIME_OF_BOOST    (ADDR_TIME_2_OFF + 1)
+#define ADDR_MAIN_SCREEN      (ADDR_TIME_OF_BOOST + 1)
+#define ADDR_PID_ENABLE       (ADDR_MAIN_SCREEN + 1)
+#define ADDR_BEEP_ENABLE      (ADDR_PID_ENABLE + 1)
+#define ADDR_VOLTAGE_VALUE    (ADDR_BEEP_ENABLE + 1)
+#define ADDR_QC_ENABLE        (ADDR_VOLTAGE_VALUE + 1)
 #define ADDR_WAKEUP_THRESHOLD (ADDR_QC_ENABLE + 1)
-#define ADDR_CURRENT_TIP (ADDR_WAKEUP_THRESHOLD + 1)
-#define ADDR_NUMBER_OF_TIPS (ADDR_CURRENT_TIP + 1)
+#define ADDR_CURRENT_TIP      (ADDR_WAKEUP_THRESHOLD + 1)
+#define ADDR_NUMBER_OF_TIPS   (ADDR_CURRENT_TIP + 1)
 
-#define ADDR_TIP_NAME (ADDR_NUMBER_OF_TIPS + 1)
-#define ADDR_CAL_TEMP (ADDR_TIP_NAME + (TIPNAMELENGTH * TIPMAX))
+#define ADDR_TIP_NAME         (ADDR_NUMBER_OF_TIPS + 1)
+#define ADDR_CAL_TEMP         (ADDR_TIP_NAME + (TIPNAMELENGTH * TIPMAX))
 
-#define ADDR_LANGUAGE (ADDR_CAL_TEMP + (2 * CALNUM * TIPMAX))
-#define ADDR_HAND_SIDE (ADDR_LANGUAGE + 1)
+#define ADDR_LANGUAGE         (ADDR_CAL_TEMP + (2 * CALNUM * TIPMAX))
+#define ADDR_HAND_SIDE        (ADDR_LANGUAGE + 1)
 
-#define ADDR_EEPROM_SIZE (ADDR_HAND_SIDE + 1)
+#define ADDR_EEPROM_SIZE      (ADDR_HAND_SIDE + 1)
 
 extern uint16_t DefaultTemp;
 extern uint16_t SleepTemp;
@@ -49,8 +49,7 @@ extern uint16_t CalTemp[TIPMAX][CALNUM];
 extern uint8_t language;
 extern uint8_t hand_side;
 
-bool write_default_EEPROM()
-{
+bool write_default_EEPROM() {
   Serial.println("Writing default config to EEPROM");
 
   EEPROM.writeUShort(ADDR_DEFAULT_TEMP, TEMP_DEFAULT);
@@ -73,40 +72,29 @@ bool write_default_EEPROM()
   CalTemp[0][2] = TEMP360;
   CalTemp[0][3] = TEMPCHP;
 
-  strncpy(TipName[0], TIPNAME, TIPNAMELENGTH);
-  TipName[0][TIPNAMELENGTH - 1] = '\0';
+  memset(TipName[0], 0, TIPNAMELENGTH);
+  strncpy(TipName[0], TIPNAME, TIPNAMELENGTH - 1);
 
-  for (uint8_t i = 0; i < 1; i++)
-  {
-    EEPROM.writeString(ADDR_TIP_NAME + i * TIPNAMELENGTH, TipName[i]);
-    for (uint8_t j = 0; j < CALNUM; j++)
-    {
-      EEPROM.writeUShort(ADDR_CAL_TEMP + i * 2 * CALNUM + j * 2, CalTemp[i][j]);
-    }
+  EEPROM.writeBytes(ADDR_TIP_NAME, TipName[0], TIPNAMELENGTH);
+  for (uint8_t j = 0; j < CALNUM; j++) {
+    EEPROM.writeUShort(ADDR_CAL_TEMP + j * 2, CalTemp[0][j]);
   }
 
   EEPROM.writeUChar(ADDR_LANGUAGE, DEFAULT_LANGUAGE);
   EEPROM.writeUChar(ADDR_HAND_SIDE, DEFAULT_HAND_SIDE);
-
   EEPROM.writeUInt(ADDR_SYSTEM_INIT_FLAG, VERSION_NUM);
 
-  if (EEPROM.commit())
-  {
+  if (EEPROM.commit()) {
     Serial.println("Default config Done");
     return true;
   }
-  else
-  {
-    Serial.println("Default config Failed");
-    return false;
-  }
+  Serial.println("Default config Failed");
+  return false;
 }
 
-bool init_EEPROM()
-{
+bool init_EEPROM() {
   Serial.println("Initialising EEPROM");
-  if (!EEPROM.begin(ADDR_EEPROM_SIZE))
-  {
+  if (!EEPROM.begin(ADDR_EEPROM_SIZE)) {
     Serial.println("Failed to initialise EEPROM");
     return false;
   }
@@ -114,8 +102,7 @@ bool init_EEPROM()
   return true;
 }
 
-bool update_EEPROM()
-{
+bool update_EEPROM() {
   Serial.println("Updating EEPROM");
 
   EEPROM.writeUShort(ADDR_DEFAULT_TEMP, DefaultTemp);
@@ -133,43 +120,31 @@ bool update_EEPROM()
   EEPROM.writeUChar(ADDR_CURRENT_TIP, CurrentTip);
   EEPROM.writeUChar(ADDR_NUMBER_OF_TIPS, NumberOfTips);
 
-  for (uint8_t i = 0; i < NumberOfTips; i++)
-  {
-    EEPROM.writeString(ADDR_TIP_NAME + i * TIPNAMELENGTH, TipName[i]);
-    for (uint8_t j = 0; j < CALNUM; j++)
-    {
+  for (uint8_t i = 0; i < NumberOfTips; i++) {
+    EEPROM.writeBytes(ADDR_TIP_NAME + i * TIPNAMELENGTH, TipName[i], TIPNAMELENGTH);
+    for (uint8_t j = 0; j < CALNUM; j++) {
       EEPROM.writeUShort(ADDR_CAL_TEMP + i * 2 * CALNUM + j * 2, CalTemp[i][j]);
     }
   }
 
   EEPROM.writeUChar(ADDR_LANGUAGE, language);
   EEPROM.writeUChar(ADDR_HAND_SIDE, hand_side);
-
   EEPROM.writeUInt(ADDR_SYSTEM_INIT_FLAG, VERSION_NUM);
 
-  if (EEPROM.commit())
-  {
+  if (EEPROM.commit()) {
     Serial.println("EEPROM Update Done");
     return true;
   }
-  else
-  {
-    Serial.println("EEPROM Update Failed");
-    return false;
-  }
+  Serial.println("EEPROM Update Failed");
+  return false;
 }
 
-bool read_EEPROM()
-{
+bool read_EEPROM() {
   Serial.println("Reading EEPROM");
 
-  if (EEPROM.readUInt(ADDR_SYSTEM_INIT_FLAG) != VERSION_NUM)
-  {
-    Serial.println("System didn't initialise");
-    if (!write_default_EEPROM()) {
-      Serial.println("Failed to write default EEPROM");
-      return false;
-    }
+  if (EEPROM.readUInt(ADDR_SYSTEM_INIT_FLAG) != VERSION_NUM) {
+    Serial.println("System uninitialized or version mismatch; resetting");
+    if (!write_default_EEPROM()) return false;
   }
 
   bool dirty = false;
@@ -189,13 +164,13 @@ bool read_EEPROM()
   hand_side = EEPROM.readUChar(ADDR_HAND_SIDE);
 
   uint8_t rawPID = EEPROM.readUChar(ADDR_PID_ENABLE);
-  if (rawPID <= 1) { PIDenable = rawPID; } else { PIDenable = PID_ENABLE; dirty = true; }
+  if (rawPID <= 1) PIDenable = rawPID; else { PIDenable = PID_ENABLE; dirty = true; }
   
   uint8_t rawBeep = EEPROM.readUChar(ADDR_BEEP_ENABLE);
-  if (rawBeep <= 1) { beepEnable = rawBeep; } else { beepEnable = BEEP_ENABLE; dirty = true; }
+  if (rawBeep <= 1) beepEnable = rawBeep; else { beepEnable = BEEP_ENABLE; dirty = true; }
   
   uint8_t rawQC = EEPROM.readUChar(ADDR_QC_ENABLE);
-  if (rawQC <= 1) { QCEnable = rawQC; } else { QCEnable = QC_ENABLE; dirty = true; }
+  if (rawQC <= 1) QCEnable = rawQC; else { QCEnable = QC_ENABLE; dirty = true; }
 
   if (DefaultTemp < TEMP_MIN || DefaultTemp > TEMP_MAX) { DefaultTemp = TEMP_DEFAULT; dirty = true; }
   if (SleepTemp < 50 || SleepTemp > TEMP_MAX) { SleepTemp = TEMP_SLEEP; dirty = true; }
@@ -212,14 +187,12 @@ bool read_EEPROM()
   if (hand_side > 1) { hand_side = DEFAULT_HAND_SIDE; dirty = true; }
   if (MainScrType > 1) { MainScrType = MAINSCREEN; dirty = true; }
 
-  for (uint8_t i = 0; i < NumberOfTips; i++)
-  {
-    memset(TipName[i], 0, sizeof(TipName[i]));
-    EEPROM.readBytes(ADDR_TIP_NAME + i * TIPNAMELENGTH, TipName[i], TIPNAMELENGTH - 1);
+  for (uint8_t i = 0; i < NumberOfTips; i++) {
+    memset(TipName[i], 0, TIPNAMELENGTH);
+    EEPROM.readBytes(ADDR_TIP_NAME + i * TIPNAMELENGTH, TipName[i], TIPNAMELENGTH);
     TipName[i][TIPNAMELENGTH - 1] = '\0';
 
-    for (uint8_t j = 0; j < CALNUM; j++)
-    {
+    for (uint8_t j = 0; j < CALNUM; j++) {
       CalTemp[i][j] = EEPROM.readUShort(ADDR_CAL_TEMP + i * 2 * CALNUM + j * 2);
     }
 
@@ -246,4 +219,4 @@ bool read_EEPROM()
   return true;
 }
 
-#endif
+#endif // UTILS_EEPROM_H
